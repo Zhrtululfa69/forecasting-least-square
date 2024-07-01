a20a4c1af7cf81cf58385df732162cd471c4eced
zaharatul ulfa <74602940+Zhrtululfa69@users.noreply.github.com>
2024-07-01 14:44:36 +0700

    Create head.php

---
+++ page/layout/head.php
@@ -0,0 +1,17 @@
+<head>
+ 	<meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Forecasting</title>
+     <link href="" rel="icon">
+     <!-- Main CSS-->
+     <link rel="stylesheet" type="text/css" href="../assets/css/styles.css?1395">
+     <!-- Font-icon css-->
+     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link rel="stylesheet" href="../assets/css/plugins/toastr/toastr.min.css">
+     <!-- sweetalert -->
+     <link rel="stylesheet" href="../assets/css/plugins/sweetalert/bootstrap-4.min.css">
+	<!-- Select2 -->
+    <link href="../assets/css/plugins/select2/select2.min.css" rel="stylesheet">
+    
+ </head>
